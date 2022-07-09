ef3ec888d9147fd4940a64d4b5b1eb0f90817aec
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2022-07-09 11:28:49 +0200

    add product schema

--- main.py
+++ main.py
@@ -10,6 +10,37 @@ client= MongoClient(connect_str)
 production = client.production
 colletions = production.list_collection_names()
 
-schema = json.load(open("schemas/producent_schema.json"))
+schema1 = json.load(open("schemas/producent_schema.json"))
+schema2 = json.load(open("schemas/product_schema.json"))
 production.create_collection("producent")
-production.command("collMod", "producent", validator = schema)
+production.create_collection("product")
+production.command("collMod", "producent", validator = schema1)
+production.command("collMod", "product", validator = schema2)
+
+def push_sample_data():
+    producent = [
+        {
+            "Name" : "test",
+            "NIP" : "123456789",
+            "Address": {
+                "Voivodeship" : "Świętokrzyskie",
+                "City" : "Sandomierz",
+                "Street" : "Kobierniki",
+                "Building_Bumber" : "138"
+            }
+        }
+    ]
+    pr_id = production.producent.insert_many(producent)
+    print(pr_id.inserted_ids)
+    product = [
+        {
+            "Name" : "TEST",
+            "Mass" : 100,
+            "Mass_Unit" : "kg",
+            "Description" : "DUPA",
+            "Producent_ID" : pr_id.inserted_ids[0]
+        }
+    ]
+    production.product.insert_many(product)
+
+push_sample_data()
