1a553e8c78091d5c5e1a833a734f2740f1e63d09
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2022-07-09 13:21:28 +0200

    Add store schema.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 from pymongo import MongoClient
 import json
+from datetime import datetime
 load_dotenv(find_dotenv())
 password = os.environ.get("MONGODB_PWD")
 connect_str = f"""mongodb+srv://mariuszpaluch:{password}@myprojects.z9jwiv9.mongodb.net/?retryWrites=true&w=majority&authSource=admin"""
@@ -12,11 +13,13 @@ colletions = production.list_collection_names()
 
 schema1 = json.load(open("schemas/producent_schema.json"))
 schema2 = json.load(open("schemas/product_schema.json"))
+schema3 = json.load(open("schemas/store_schema.json"))
 production.create_collection("producent")
 production.create_collection("product")
+production.create_collection("store")
 production.command("collMod", "producent", validator = schema1)
 production.command("collMod", "product", validator = schema2)
-
+production.command("collMod", "store", validator = schema3)
 def push_sample_data():
     producent = [
         {
@@ -43,4 +46,24 @@ def push_sample_data():
     ]
     production.product.insert_many(product)
 
+    store = [
+        {
+            "Name" : "TEST",
+            "Description" : "TEST",
+            "Manager" : {
+                "First_Name" : "TESTOWY",
+                "Name" : "TEST",
+                "Employment_Date" : datetime.now(),
+                "Telephone_Number" : "123123123123"
+            },
+            "Description" : "DUPA",
+            "Address": {
+                "Voivodeship" : "Świętokrzyskie",
+                "City" : "Sandomierz",
+                "Street" : "Kobierniki",
+                "Building_Bumber" : "138"
+            }
+        }
+    ]
+    production.store.insert_many(store)
 push_sample_data()
