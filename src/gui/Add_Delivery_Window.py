2e2da9d6200d5da73360b690409bda4deca06e51
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2022-07-12 15:36:25 +0200

    Add ui project to add product window

---
+++ src/gui/Add_Delivery_Window.py
@@ -0,0 +1,5 @@
+from PySide2.QtWidgets import QMainWindow
+
+class Add_Delivery_Window(QMainWindow):
+    def __init__(self, parent = None):
+        super().__init__(parent)
---
+++ src/gui/Add_Product_Window.py
@@ -0,0 +1,5 @@
+from PySide2.QtWidgets import QMainWindow
+
+class Add_Product_Window(QMainWindow):
+    def __init__(self, parent = None):
+        super().__init__(parent)
